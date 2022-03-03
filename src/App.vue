e786b6ec6c4e2ce4717bb4bc2a90430b5c681a7d
wujiess <yanmingpc123@163.com>
2022-03-03 14:51:27 +0800

    重新创建项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts">
+import { ref, provide } from 'vue';
+
+export default {
+  name: "App",
+  setup(){
+    const asideVisible = ref(false);
+    provide('asideVisible', asideVisible);
+  }
+};
+</script>
