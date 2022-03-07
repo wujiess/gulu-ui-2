e1b888f082ebb3a2ecd38a8c0f3891884f76babb
yanmingpc123 <yanmingpc123@163.com>
2022-03-07 23:20:39 +0800

    优化 css 代码

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -24,27 +24,40 @@ button {
   height: $h;
   width: $h * 2;
   border: none;
-  background-color: grey;
+  background-color: #bfbfbf;
   border-radius: calc($h / 2);
   position: relative;
-}
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background-color: white;
-  border-radius: calc($h2 / 2);
-  transition: left 250ms;
-}
-button.checked {
-  background-color: blue;
-}
-button.checked > span {
-  left: calc(100% - #{$h2} - 2px);
-}
-button:focus {
-  outline: none;
+
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background-color: white;
+    border-radius: calc($h2 / 2);
+    transition: all 250ms;
+  }
+
+  &.checked {
+    background-color: #1890ff;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &:focus {
+    outline: none;
+  }
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
+  }
+  &.checked.active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
+  }
 }
 </style>
